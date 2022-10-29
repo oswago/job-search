9f82627f565e6578fc03b90e6d65aea7542cfa1e
elly <omondyelly@gmail.com>
2022-10-29 21:56:03 +0300

    Jest test samples added

---
+++ src/playground.js
@@ -0,0 +1,12 @@
+export const evenOdd = (number) => {
+  if (number % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+};
+
+export const multiply = (a, b) => {
+  var res = a * b;
+  return res;
+};
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "brand-grey-1": "#dadce0",
+        "brand-blue-1": "#1967d2",
+        "brand-green-1": "#137333",
+      },
+    },
+  },
+  plugins: [],
+};
